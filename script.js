dc6d9865a6c8cf3b7141023027c8b3c3e3ad0f31
Usman Shakeel <107518623+Usman-shakeel@users.noreply.github.com>
2023-04-14 08:52:01 +0500

    1st commit

---
+++ script.js
@@ -0,0 +1,67 @@
+var screenValue;
+
+var multiplication;
+var division;
+var subtraction;
+var addition;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const inputField = document.querySelector('#screen');
+  screenValue = inputField;
+  console.log(screenValue.value);
+});
+
+function clearScreen() {
+  screenValue.value = "";
+}
+
+function button(number) {
+  screenValue.value += number;
+}
+
+function point() {
+  if (!screenValue.value.includes('.')) {
+    screenValue.value += '.';
+  }
+}
+
+function percent() {
+  var currentValue = parseFloat(screenValue.value);
+  var percentValue = currentValue / 100;
+  screenValue.value = percentValue.toFixed(2) + "%";
+}
+
+function division() {
+  if (!screenValue.value.includes('/')) {
+    screenValue.value += '/';
+  }
+}
+
+function multiply() {
+  if (!screenValue.value.includes('*')) {
+    screenValue.value += '*';
+  }
+}
+
+function addition() {
+  if (!screenValue.value.includes('+')) {
+    screenValue.value += '+';
+  }
+}
+
+function subtraction() {
+  if (!screenValue.value.includes('-')) {
+    screenValue.value += '-';
+  }
+}
+
+function equalsTo() {
+    const expression = screenValue.value;
+    const result = eval(expression);
+    screenValue.value = result;  
+}
+
+function deleteLastDigit() {
+  screenValue.value = screenValue.value.slice(0, -1);
+}
+
